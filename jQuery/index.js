01c5c1edf03468fa28b436d2efcf1707f64dbaae
chouella <kanyonga.chouell@gmail.com>
2021-06-10 00:02:53 +0300

    Initial Commit

---
+++ jQuery/index.js
@@ -0,0 +1,4 @@
+$("button").on("click", function(){
+    $("h1").fadeToggle();
+   ;
+});
---
+++ javascript/javascript/DOM/index.js
@@ -0,0 +1,4 @@
+document.querySelector("input").click();
+document.getElementsByTagName("li")[2].innerHTML="chouella";
+document.querySelector("#list a").style.color="brown";
+document.querySelector(".btn").style.backgroundColor="yellow";
